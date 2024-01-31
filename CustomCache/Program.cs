9d5a70d5a18345e3ffbd4adec337202c1c5c83e7
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-01-31 15:09:01 +0100

    add struttura base

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(dataDownloader.DownloadData("id1");
+Console.WriteLine(dataDownloader.DownloadData("id2");
+Console.WriteLine(dataDownloader.DownloadData("id3");
+Console.WriteLine(dataDownloader.DownloadData("id1");
+Console.WriteLine(dataDownloader.DownloadData("id3");
+Console.WriteLine(dataDownloader.DownloadData("id1");
+Console.WriteLine(dataDownloader.DownloadData("id2");
+
+
+
+public class SlowDataDownloader : IDataDownloader
+{
+    public string DownloadData(string resourceId)
+    {
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
+    }
+}

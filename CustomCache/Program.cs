703527bf061a207b3bb3011f7cf96d7e5019b860
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-01-31 15:21:02 +0100

    add implementazione interfaccia e classe

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,12 +1,21 @@
-﻿Console.WriteLine(dataDownloader.DownloadData("id1");
-Console.WriteLine(dataDownloader.DownloadData("id2");
-Console.WriteLine(dataDownloader.DownloadData("id3");
-Console.WriteLine(dataDownloader.DownloadData("id1");
-Console.WriteLine(dataDownloader.DownloadData("id3");
-Console.WriteLine(dataDownloader.DownloadData("id1");
-Console.WriteLine(dataDownloader.DownloadData("id2");
+﻿var dataDownloader = new SlowDataDownloader();
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
 
 
+public interface IDataDownloader
+{
+    public string DownloadData(string resource);
+}
 
 public class SlowDataDownloader : IDataDownloader
 {
@@ -16,3 +25,4 @@ public class SlowDataDownloader : IDataDownloader
         return $"Some data for {resourceId}";
     }
 }
+
